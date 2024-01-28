9b224a893fddd8e9c47f9bb722a81e48b1049395
adhinnnugroho <adhinnnugroho@gmail.com>
2024-01-28 18:01:17 +0700

    refactor: view breadcrumbs

--- resources/views/livewire/breadcrumbs/simple-breadcrumbs.blade.php
+++ resources/views/livewire/breadcrumbs/simple-breadcrumbs.blade.php
@@ -4,11 +4,11 @@
             <ol class="inline-flex items-center space-x-1 md:space-x-2" id="url_header">
                 @foreach ($data as $title => $link)
                     @if (!is_null($link))
-                        <li><a href="{{ $link }}" class="text-gray-500 hover:text-gold-700">{{ $title }}</a>
+                        <li><a href="{{ $link }}" class="text-gray-500 hover:text-black">{{ $title }}</a>
                         </li>
                         <li><span class="text-gray-500 mx-2"><i class="fas fa-angle-right"></i></span></li>
                     @else
-                        <li class="text-gold-500">{{ $title }}</li>
+                        <li class="text-black font-bold">{{ $title }}</li>
                     @endif
                 @endforeach
             </ol>
